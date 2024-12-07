9a12465597fcb8eab260569b86ee650a7c0416b4
Adam Shen <46703463+adamoshen@users.noreply.github.com>
2024-12-07 11:48:48 -0500

    Update azure_init.R
    Programmatically determine whether user is in RStudio or in a terminal session. If access to a web browser is available, it is more convenient to use.

--- R/azure_init.R
+++ R/azure_init.R
@@ -2,5 +2,5 @@ token <- AzureAuth::get_azure_token(
   resource = "https://cbspeeches1.dfs.core.windows.net/",
   tenant = "adamshen1live.onmicrosoft.com",
   app = "04b07795-8ddb-461a-bbee-02f9e1bf7b46",
-  auth_type = "device_code"
+  auth_type = ifelse(rstudioapi::isAvailable(), "authorization_code", "device_code")
 )
